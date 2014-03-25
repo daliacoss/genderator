b7457d9de3c2e6840ef3f6e042347b235cb97437
david <cosstropolis@gmail.com>
2014-03-25 14:45:46 -0400

    basic sqlalchemy setup

--- __init__.py
+++ __init__.py
@@ -1,5 +1,7 @@
 from flask import Flask
+
 app = Flask(__name__)
+app.config.from_pyfile("config.py")
 
 class WebFactionMiddleware(object):
     def __init__(self, app):
@@ -11,7 +13,7 @@ class WebFactionMiddleware(object):
 app.wsgi_app = WebFactionMiddleware(app.wsgi_app)
 
 import genderator.views
-
+import genderator.models
 #@app.route("/")
 #def hello():
 #    return "Hello World!"
