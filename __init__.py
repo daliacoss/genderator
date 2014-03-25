14f29f6b5da145c8dc47d0cd73cc09c6b2dd8ae1
deckman <cosstropolis@gmail.com>
2014-03-25 16:17:46 +0000

    first commit

---
+++ __init__.py
@@ -0,0 +1,17 @@
+from flask import Flask
+app = Flask(__name__)
+
+class WebFactionMiddleware(object):
+    def __init__(self, app):
+        self.app = app
+    def __call__(self, environ, start_response):
+        environ['SCRIPT_NAME'] = 'genderator'
+        return self.app(environ, start_response)
+
+app.wsgi_app = WebFactionMiddleware(app.wsgi_app)
+
+@app.route("/")
+def hello():
+    return "Hello World!"
+
+
---
+++ index.py
@@ -0,0 +1,13 @@
+import sys
+
+def application(environ, start_response):
+    output = 'Welcome to your mod_wsgi website! It uses:\n\nPython %s' % sys.version
+
+    response_headers = [
+        ('Content-Length', str(len(output))),
+        ('Content-Type', 'text/plain'),
+    ]
+
+    start_response('200 OK', response_headers)
+
+    return [output]
